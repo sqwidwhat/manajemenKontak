b875b12b22b12b042bd7b3150e39c652033b17cc
sqwidwhat <andikamelawi.net11@gmail.com>
2025-03-07 20:07:51 +0700

    menggunakan kelas (oop)

--- contactMangement.py
+++ contactMangement.py
@@ -1,39 +1,37 @@
-kontak = [
-    {'Nama': "Andi", 'HP': '0812323143', 'EMAIL': "andi@gmail.com"},
-    {'Nama': "Ane", 'HP': '0812323145', 'EMAIL': "ane@gmail.com"}
-]
+class Kontak:
+    def __init__(self):
+        self.kontak = []
 
-def meilihatKontak():
-    if kontak:
-        for idx, i in enumerate(kontak, start=1):
-            print(f'{idx}. {i["Nama"]}. {i["HP"]}. {i["EMAIL"]}')
-    else:
-        print("Kontak kosong")
-        return 1
-
-def menambahKontak():
-    nama = input("Masukan nama kontak baru : ")
-    hp = input("Masukan nomor hp: ")
-    email = input("Masukan email: ")
-    kontak.append({'Nama' : nama, 'HP' :hp, 'EMAIL' : email})
-    print("\nKontak berhasil ditambahkan")
-
-def menghapusKontak():
-    if meilihatKontak() == 1:
-        return
-    try:
-        hapusKontak = int(input("Masukan nomor kontak yang ingin anda hapus: "))
-        if 1 <= hapusKontak <= len(kontak):#Fungsi len() dalam Python digunakan untuk menghitung jumlah elemen dalam suatu objek seperti list, string, tuple, atau dictionary.
-            del kontak[hapusKontak - 1]
-            print("\nKontak berhasil dihapus")
+    def meilihatKontak(self):
+        if self.kontak:
+            for idx, i in enumerate(self.kontak, start=1):
+                print(f'{idx}. {i["Nama"]}. {i["HP"]}. {i["EMAIL"]}')
         else:
-            print("\nNomor kontak tidak valid!")
-    except ValueError:
-        print("\nMasukan angka yang valid")
+            print("Kontak kosong")
+            return 1
 
+    def menambahKontak(self):
+        nama = input("Masukan nama kontak baru : ")
+        hp = input("Masukan nomor hp: ")
+        email = input("Masukan email: ")
+        self.kontak.append({'Nama' : nama, 'HP' :hp, 'EMAIL' : email})
+        print("\nKontak berhasil ditambahkan")
 
+    def menghapusKontak(self):
+        if self.meilihatKontak() == 1:
+            return
+        try:
+            hapusKontak = int(input("Masukan nomor kontak yang ingin anda hapus: "))
+            if 1 <= hapusKontak <= len(self.kontak):#Fungsi len() dalam Python digunakan untuk menghitung jumlah elemen dalam suatu objek seperti list, string, tuple, atau dictionary.
+                del self.kontak[hapusKontak - 1]
+                print("\nKontak berhasil dihapus")
+            else:
+                print("\nNomor kontak tidak valid!")
+        except ValueError:
+            print("\nMasukan angka yang valid")
 
 def main():
+    kontak = Kontak()
     while True:
         print("")
         print("1. Melihat Kontak")
@@ -43,11 +41,11 @@ def main():
         pilihan = input("Masukan Pilihan: ")
 
         if pilihan == '1':
-            meilihatKontak()
+            kontak.meilihatKontak()
         elif pilihan == '2':
-            menambahKontak()
+            kontak.menambahKontak()
         elif pilihan == '3':
-            menghapusKontak()
+            kontak.menghapusKontak()
         elif pilihan == '4':
             print("\nKeluar dari program. Terima kasih!")
             break
